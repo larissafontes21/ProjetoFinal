da50050665751927a27b6020f1a935ce58491ba1
Larissa Fontes <larissa.fontes21@gmail.com>
2022-12-10 14:53:34 -0300

    add swagger

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require("../swagger/swagger_output.json")
+
 require("dotenv-safe").config(); //* Ver esse caminho
 const express = require("express");
 const cors = require("cors");
@@ -13,5 +16,6 @@ mongoose.connect();
 
 app.use("/saibamulher/ongs",ongsRoutes);
 app.use("/saibamulher/atividades",atividadesRoutes);
+app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 module.exports = app;
